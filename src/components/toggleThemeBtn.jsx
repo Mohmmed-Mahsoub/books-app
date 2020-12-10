503296ded6641dcb605a341189a8cd370c4f1228
unknown <mohamedmahsoub181@gmail.com>
2020-12-10 10:21:32 +0200

    style the toggle theme btn and make color and background dynamicly depend on the theme

--- src/components/toggleThemeBtn.jsx
+++ src/components/toggleThemeBtn.jsx
@@ -2,8 +2,14 @@ import React, { useContext } from "react";
 import { ThemeContext } from "./../contexts/themeContext";
 const ToggleThemeBtn = () => {
   const { theme, dispatch } = useContext(ThemeContext);
+  const { isThemeLight, light, dark } = theme;
+  const themeData = isThemeLight ? light : dark;
   return (
-    <button onClick={() => dispatch({ type: "CHANGE_THEME" })}>
+    <button
+      className="toggle-theme"
+      onClick={() => dispatch({ type: "CHANGE_THEME" })}
+      style={{ background: themeData.bg, color: themeData.textColor }}
+    >
       Toggle Theme
     </button>
   );
