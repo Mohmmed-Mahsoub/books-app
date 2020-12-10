7e3620f1645a346e8e8c0352f665321df9999c84
unknown <mohamedmahsoub181@gmail.com>
2020-12-10 09:59:40 +0200

    access theme context data from the components which you want to make it's style to be dynamicly depends on if the theme light or dark

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import React, { useContext } from "react";
 import Navbar from "./components/navbar";
 import BookList from "./components/bookList";
 import NewBookForm from "./components/newBookForm";
 import BooksContextProvider from "./contexts/booksContext";
+import { ThemeContext } from "./contexts/themeContext";
 
 function App() {
+  const { theme } = useContext(ThemeContext);
+  const { isThemeLight, light, dark } = theme;
+  const themeData = isThemeLight ? light : dark;
   return (
-    <div className="App">
+    <div
+      className="App"
+      style={{ background: themeData.bg, color: themeData.textColor }}
+    >
       <BooksContextProvider>
         <Navbar />
         <BookList />
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,9 +1,14 @@
 import React, { useContext } from "react";
 import { BooksContext } from "../contexts/booksContext";
+import { ThemeContext } from "./../contexts/themeContext";
+
 const Navbar = () => {
   const { books } = useContext(BooksContext);
+  const { theme } = useContext(ThemeContext);
+  const { isThemeLight, light, dark } = theme;
+  const themeData = isThemeLight ? light : dark;
   return (
-    <div className="navbar">
+    <div className="navbar" style={{ background: themeData.ui }}>
       <h1>Ninja Reading List</h1>
       <p>Currently you have {books.length} books to get through...</p>
     </div>
